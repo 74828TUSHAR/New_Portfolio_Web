2b43dff0b63f020a9f6aff5c8ba8dd0d009f957d
Tushar Raut <ertusharraut@gmail.com>
2024-07-01 10:39:30 +0530

    Initial commit

---
+++ core/static/core/js/all.js
@@ -0,0 +1,12 @@
+var Typed = new Typed(".text", {
+  strings: [
+    "Full-Stack Developer",
+    "Front-End Developer",
+    "Back-End Developer",
+    "Database Handler",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
